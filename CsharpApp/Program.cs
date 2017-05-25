c52d25d5abe417ded1102a5cfedaa7b14bc2a8d6
Vincent B <evilznet@gmail.com>
2017-05-25 17:49:17 +0200

    first commit

---
+++ CsharpApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fsharp.Lib;
+using Microsoft.FSharp.Core;
+using Console = Colorful.Console;
+using C = System.Drawing.Color;
+
+namespace CsharpApp
+{
+    class Program
+    {
+        private static void WriteTitle(string title)
+        {
+            Console.WriteLine($"\n\n##### {title} #####\n", C.Lime);
+        }
+        static void Main(string[] args)
+        {
+            WriteTitle("Class");
+            var obj = new Product(1,"phone");
+            Console.WriteLine(obj);
+            Console.WriteLine(obj.Id);
+            Console.WriteLine(obj.IsExpensive);
+            Console.WriteLine(obj.Price);
+            Console.WriteLine(obj.CanBeSoldTo("fr"));
+
+            WriteTitle("Record");
+            var record = new Fsharp.Lib.Person("John", "Doe", new DateTime(1980, 01, 01));
+            var record2 = new Fsharp.Lib.Person("John", "Doe", new DateTime(1980, 01, 01));
+            Console.WriteLine(record);
+            Console.WriteLine(record.Equals(record2));
+            Console.WriteLine(record.LastName);
+            record.LastName = "Buzz";
+            Console.WriteLine(record.LastName);
+            Console.WriteLine(record.Equals(record2));
+            Console.WriteLine(record.FullName);
+            Console.WriteLine(record.IsBirthday());
+
+            WriteTitle("Enum & union type");
+            Console.WriteLine(Color.Red);
+            Console.WriteLine(ColorEnum.Blue);
+            Console.WriteLine(PaymentMethod.NewCreditCard(CardType.Visa, CardNumber.NewCardNumber("09876543234")));
+
+            WriteTitle("pattern matching");
+            Console.WriteLine(PureFsharp.IntPatternMatching(3));
+            Console.WriteLine(PureFsharp.TypeTesting(record));
+
+            WriteTitle("Option Type");
+            var option = PureFsharp.GetOptionType;
+            var vlue = option.Value;
+            Console.WriteLine(option);
+            Console.WriteLine(vlue);
+            var opt = FSharpOption<string>.Some("toto");
+            Console.WriteLine(PureFsharp.UseOptionType(opt));
+
+            WriteTitle("Extension method");
+            var even = 1.IsEven2();
+            Console.WriteLine(even);
+
+            WriteTitle("Method using Active patterns");
+            Console.WriteLine(PureFsharp.EvenOrOdd(3));
+
+            WriteTitle("Tuples");
+            var (a, b, c) = PureFsharp.UseTuples(3, 5, 10);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Fsharp.Lib/Lib.fs
@@ -0,0 +1,128 @@
+﻿namespace Fsharp.Lib
+
+open System
+open System.Runtime.CompilerServices
+
+/// Example of a simple class
+type Product(id, name, price) = 
+
+    /// immutable Id property
+    member this.Id = id
+
+    /// mutable Name property
+    member val Name = name with get,set
+
+    /// mutable Price property
+    member val Price = price with get,set
+
+    /// secondary constructor
+    new(id,name) = Product(id,name,Product.DefaultPrice)
+
+    /// True if price > 10.00
+    member this.IsExpensive = this.Price > 10.00
+
+    /// Example of method
+    member this.CanBeSoldTo(countryCode) = 
+        match countryCode with
+        | "US" 
+        | "CA" 
+        | "UK" -> true
+        | "RU" -> false
+        | _  -> false   //all others
+    
+    /// Example of static property
+    static member DefaultPrice = 9.99
+    
+/// Definition of a Person
+type Person = {
+    FirstName: string
+    mutable LastName: string
+    DateOfBirth: DateTime
+    }
+    with 
+    member this.FullName = 
+        this.FirstName + " " + this.LastName
+
+    member this.IsBirthday() = 
+        DateTime.Today.Month = this.DateOfBirth.Month 
+        && DateTime.Today.Day = this.DateOfBirth.Day
+
+
+type Color = Red | Green | Blue
+type ColorEnum = Red=1 | Green=2 | Blue=3
+
+
+type CheckNumber = CheckNumber of int
+type CardType = MasterCard | Visa
+type CardNumber = CardNumber of string
+
+/// PaymentMethod is cash, check or card
+[<NoComparisonAttribute>]
+type PaymentMethod = 
+    /// Cash needs no extra information
+    | Cash
+    /// Check needs a CheckNumber 
+    | Check of CheckNumber 
+    /// CreditCard needs a CardType and CardNumber 
+    | CreditCard of CardType * CardNumber 
+
+// to be used in C#
+[<Extension>]
+type IntExtension =
+    [<Extension>]
+    static member IsEven2(i: int) = i % 2 = 0
+
+
+
+module PureFsharp =
+    open System.Runtime.CompilerServices
+
+    let IntPatternMatching x = 
+        match x with
+        | 1 -> "iphone"
+        | 2 -> "nexus"
+        | 3 -> "winphone"
+        | 4 -> "Xiaomi"
+        // example of guard 
+        | e when x%2 = 0 -> "even" 
+        // wildcard
+        | _ -> "other"
+
+
+    /// demonstrates some type-testing pattern matching
+    let TypeTesting obj = 
+        match box obj with
+        | :? string as s -> 
+            sprintf "Obj is string with value %s" s
+        | :? int as i -> 
+            sprintf "Obj is int with value %i" i
+        | :? Person as p -> 
+            sprintf "Obj is Person with name %s" p.FullName
+        | _ -> 
+        sprintf "Obj is something else"
+
+    let GetOptionType = 
+        Some "aaa"
+
+    let UseOptionType o = 
+        match o with
+        | Some a -> "has a value"
+        | None -> "no value"
+
+    // not visible in C#
+    type System.Int32 with
+        member this.IsEven = this % 2 = 0
+
+
+    
+    // active patterns
+    let (|Even|Odd|) input = if input % 2 = 0 then Even else Odd
+
+    let EvenOrOdd n = 
+        match n with
+        | Even -> sprintf "%i is even" n
+        | Odd -> sprintf "%i is odd" n
+    
+    let UseTuples (a , b) c = 
+        ( a + 1, b - 1, c * c)
+   
